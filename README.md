2cf448b7c7cf93d4665e8d505214328fe0ab6101
Ethan Wong <43189717+to-ethan@users.noreply.github.com>
2020-05-10 23:51:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AuctionSniper
+ Implementation of code from the book, "Growing Object-Oriented Software, Guided by Tests"
---
+++ src/test/java/endtoend/AuctionSniperEndToEndTest.java
@@ -0,0 +1,29 @@
+package endtoend;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    // Additional Cleanup
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showSniperHasLostAuction();
+    }
+}
